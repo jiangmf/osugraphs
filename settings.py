8e8a5cef644a32f0a01ddf70f0a9ddc117779ce8
David Jiang <nisja123@gmail.com>
2016-04-28 16:07:55 -0400

    mysql

--- settings.py
+++ settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'osugraphs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'osugraphs',
-#        'USER': 'postgres',
-#        'PASSWORD': '359527497',
-#        # 'PASSWORD': 'Rh!n0DB',
-#        'HOST': '127.0.0.1',
-#        'PORT': '5432',
-#    },
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nisja123_osugraphs',
+        'USER': 'nisja123_osu',
+        #'PASSWORD': '359527497',
+        'PASSWORD': 'Rh!n0DB',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    },
 }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'mydatabase',
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
